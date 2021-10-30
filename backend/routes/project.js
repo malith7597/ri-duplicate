4666e936d82316f472c532e3df445d9d063ac946
Akalanka-1996 <kasunakalanka4@gmail.com>
2021-10-30 16:11:18 +0530

    project controller added

--- backend/routes/project.js
+++ backend/routes/project.js
@@ -2,11 +2,18 @@ const router = require('express').Router();
 let Project = require('../model/project.model');
 // let Task = require('../model/task.model')
 
-
+// get projects
 router.route('/').get((req,res) => {
     Project.find()
         .then(project => res.json(project))
         .catch(err => res.status(400).json('Error'+err));
+})
+
+// get specific project
+router.route('/:id').get((req,res) => {
+    Project.findById(req.params.id)
+        .then(project => res.json(project))
+        .catch(err => res.status(400).json('Error')+err);
 })
 
 //create new project
@@ -36,6 +43,36 @@ router.route('/add').post((req,res) => {
 
 
 
+})
+
+// update a project
+
+router.route('/update/:id').post((req,res) => {
+    Project.findById(req.params.id)
+        .then(project => {
+            project.avenueName = req.body.avenueName;
+            project.projectName = req.body.projectName;
+            project.startDate = Date.parse(req.body.startDate);
+            project.duration = req.body.duration;
+            project.task = req.body.task;
+            project.projectStatus = req.body.projectStatus;
+            project.projectMilestones = req.body.projectMilestones;
+
+            project.save()
+            .then(() => res.json('Project Updated!'))
+            .catch(err => res.status(400).json('Error'+err))
+
+
+        })
+
+})
+
+// delete project
+
+router.route('/:id').delete((req,res) => {
+    Project.findByIdAndDelete(req.params.id)
+        .then(project => res.json('Project Deleted!'))
+        .catch(err => res.status(400).json('Error'+err))
 })
 
 module.exports = router;
