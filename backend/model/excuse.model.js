91507f06bfcec74fef68d444fa0d6e450a0e1a27
Akalanka-1996 <kasunakalanka4@gmail.com>
2021-12-12 00:17:32 +0530

    Revert "Revert "meeting added""
    This reverts commit bc986f6fd2f5799cba54991f67ec5efd6a712f23.

--- backend/model/excuse.model.js
+++ backend/model/excuse.model.js
@@ -5,10 +5,14 @@ const Schema = mongoose.Schema;
 const excuseSchema = new Schema({
     RACUOK_ID:{
         type:String,
-         required:true
+        required:true
     },
     memberName:{
         type:String,  
+        required:true
+    },
+    avenue:{
+        type:String,
         required:true
     },
     reason:{
