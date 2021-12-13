60f8e9960f6305b62405993058dd273b5d7413c9
Akalanka-1996 <kasunakalanka4@gmail.com>
2021-12-13 17:13:07 +0530

    meeting model added

---
+++ backend/model/meeting.model.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+const meetingSchema = mongoose.Schema({
+    meetingLink:{
+        type:String,
+        required:true
+    },
+    time:{
+        type:String,
+        required:true
+    },
+    date:{
+        type:Date,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    isFinished:{
+        type:Boolean,
+        required:true,
+        default:false,
+    },
+    avenueName:{
+        type:String,
+    },
+    virtualBg:{
+        type:String,
+        
+    },
+    member:{
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "Member"
+    },
+}, {
+    timestamps:true
+})
+
+const Meeting = mongoose.model('Meeting', meetingSchema)
+
+module.exports = Meeting
